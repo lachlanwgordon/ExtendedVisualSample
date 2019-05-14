690fbb04267c19a65184fc3c64796b03d2196801
Lachlan Gordon <lachlan@cookingthebooks.com.au>
2019-05-14 21:31:02 +1000

    Implemented PickerRenderers to show Placeholders and hints in CustomVisual

--- VisualExtension.Android/Renderers/TitledMaterialPickerRenderer.cs
+++ VisualExtension.Android/Renderers/TitledMaterialPickerRenderer.cs
@@ -1,23 +1,33 @@
-﻿//using System;
-//using Android.Content;
-//using VisualExtension;
-//using VisualExtension.Droid.Renderers;
-//using Xamarin.Forms;
-//using Xamarin.Forms.Material.Android;
-//using Xamarin.Forms.Platform.Android;
+﻿using System;
+using Android.Content;
+using VisualExtension;
+using VisualExtension.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Material.Android;
 
-//[assembly: ExportRenderer(typeof(Picker), typeof(TitledMaterialPickerRenderer), new[] {typeof(CustomVisual) } ) ]
-//namespace VisualExtension.Droid.Renderers
-//{
-//    public class TitledMaterialPickerRenderer : MaterialPickerRenderer
-//    {
-//        public TitledMaterialPickerRenderer(Context context) : base(context)
-//        {
-//        }
+[assembly: ExportRenderer(typeof(Picker), typeof(TitledMaterialPickerRenderer), new[] { typeof(CustomVisual) })]
+namespace VisualExtension.Droid.Renderers
+{
+    public class TitledMaterialPickerRenderer : MaterialPickerRenderer
+    {
+        public TitledMaterialPickerRenderer(Context context) : base(context)
+        {
 
-//        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
-//        {
-//            base.OnElementChanged(e);
-//        }
-//    }
-//}
+        }
+
+        protected override void UpdatePlaceHolderText()
+        {
+            base.UpdatePlaceHolderText();
+            InputLayout.HintEnabled = true;
+            InputLayout.Hint = Element.Title;
+        }
+
+        MaterialPickerTextInputLayout InputLayout;
+        protected override MaterialPickerTextInputLayout CreateNativeControl()
+        {
+            InputLayout = base.CreateNativeControl();
+            return InputLayout;
+        }
+
+    }
+}
--- VisualExtension.iOS/Renderers/TitledMaterialPickerRenderer.cs
+++ VisualExtension.iOS/Renderers/TitledMaterialPickerRenderer.cs
@@ -1,18 +1,31 @@
-﻿//using System;
-//using VisualExtension;
-//using VisualExtension.iOS.Renderers;
-//using Xamarin.Forms;
-//using Xamarin.Forms.Material.iOS;
+﻿using System;
+using VisualExtension;
+using VisualExtension.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Material.iOS;
+using Xamarin.Forms.Platform.iOS;
 
-//[assembly: ExportRenderer(typeof(Button), typeof(TitledMaterialPickerRenderer), new[] {typeof(CustomVisual) } ) ]
-//namespace VisualExtension.iOS.Renderers
-//{
-//    public class TitledMaterialPickerRenderer : MaterialPickerRenderer
-//    {
-//        public TitledMaterialPickerRenderer()
-//        {
-//        }
+[assembly: ExportRenderer(typeof(Picker), typeof(TitledMaterialPickerRenderer), new[] { typeof(CustomVisual) })]
+namespace VisualExtension.iOS.Renderers
+{
+    public class TitledMaterialPickerRenderer : MaterialPickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            if(Control != null)
+            {
+                Control.Placeholder = Element.Title;
+                Control.HidesPlaceholderOnInput = false;
+            }
+        }
 
-        
-//    }
-//}
+        protected override void UpdatePlaceholder()
+        {
+            //This method intentionally blank
+            //The base implementation hides the place holder when selected
+            //This material renderer automatically shrinks the placeholder
+            //I may need to revisit this
+        }
+    }
+}
