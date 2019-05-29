343bb69b406d51918cbae1f71bd29dcee7c6d1ca
Lachlan Gordon <lachlan@cookingthebooks.com.au>
2019-05-29 16:27:37 +1000

    I found a cleaner way to declare all those renderers. Doing all that pointless subclassing was gross. Now I just declare all my renderers in one file per target project.
     - Deleted all the pointless renderers
     - Added a CustomVisualRenderersList to each target project

---
+++ VisualExtension.Android/Renderers/CustomVisualRenderersList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using VisualExtension;
+using Xamarin.Forms;
+using Xamarin.Forms.Material.Android;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(MaterialEditorRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(Entry), typeof(MaterialEntryRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(ActivityIndicator), typeof(MaterialActivityIndicatorRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(Frame), typeof(MaterialFrameRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(Slider), typeof(MaterialSliderRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(ProgressBar), typeof(MaterialProgressBarRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(Stepper), typeof(MaterialStepperRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(DatePicker), typeof(MaterialDatePickerRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(TimePicker), typeof(MaterialTimePickerRenderer), new[] { typeof(CustomVisual) })]
+namespace VisualExtension.Droid.Renderers
+{
+    public class CustomVisualRenderersList
+    {
+        public CustomVisualRenderersList()
+        {
+        }
+    }
+}
---
+++ VisualExtension.iOS/Renderers/CustomVisualRenderersList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using VisualExtension;
+using Xamarin.Forms;
+using Xamarin.Forms.Material.iOS;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(MaterialEditorRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(Entry), typeof(MaterialEntryRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(ActivityIndicator), typeof(MaterialActivityIndicatorRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(Frame), typeof(MaterialFrameRenderer), new[] {typeof(CustomVisual)})]
+[assembly: ExportRenderer(typeof(Slider), typeof(MaterialSliderRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(ProgressBar), typeof(MaterialProgressBarRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(Stepper), typeof(MaterialStepperRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(DatePicker), typeof(MaterialDatePickerRenderer), new[] { typeof(CustomVisual) })]
+[assembly: ExportRenderer(typeof(TimePicker), typeof(MaterialTimePickerRenderer), new[] { typeof(CustomVisual) })]
+namespace VisualExtension.iOS.Renderers
+{
+    public class CustomVisualRenderersList
+    {
+    }
+}
